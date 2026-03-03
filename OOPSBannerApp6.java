74ffd3e61e86c8739b44a920606712eb774ebabb
Ayush Agarwal <ayushagarwal@Ayushs-MacBook-Air-1037.local>
2026-03-03 22:28:36 +0530

    UC6

---
+++ OOPSBannerApp6.java
@@ -0,0 +1,22 @@
+public class OOPSBannerApp6 {
+
+    public static void main(String[] args) {
+
+        // Inline declaration and initialization of banner lines
+        String[] lines = {
+            String.join(" ", " *** ", "  ***  ", " ***** ", " ***** "),
+            String.join(" ", "*   *", " *   * ", "*     *", "*      "),
+            String.join(" ", "*   *", " *   * ", "*     *", "*      "),
+            String.join(" ", "*   *", " *   * ", " ***** ", " ***** "),
+            String.join(" ", "*   *", " *   * ", "*       ","     *"),
+            String.join(" ", "*   *", " *   * ", "*       ","     * "),
+            String.join(" ", " *** ", "  ***  ", "*       "," ***** ")
+        };
+
+        // Enhanced for-loop to print banner
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    
+}
+}
